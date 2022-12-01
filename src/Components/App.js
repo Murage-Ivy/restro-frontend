4288ae95c23168020d367218e153164fb0e9aebc
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-12-01 18:34:13 +0300

    Create sign up form

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import SignUp from "./Signup/SignupForm";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Restro App */}
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
